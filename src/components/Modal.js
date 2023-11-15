9b342a636e444fffc4d23a81e0486407c1150097
alpefe44 <efe1998alp@hotmail.com>
2023-11-15 18:02:21 +0300

    .

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,7 +17,7 @@ export default function ModalComp({ modal, setModalOpen }) {
                 <button className='absolute right-3 top-0 font-bold ' onClick={() => closeModal()}>X</button>
                 <div className='flex flex-col gap-y-5 w-full h-full justify-center items-center'>
                     <h1 className='text-center'>CV'mi tıklayarak indirebilirsiniz</h1>
-                    <a href='C:\Users\Alp\Desktop\cvphoto' download className='text-lg text-gray-400'><FaFilePdf /></a>
+                    <a href='/cvalpefeakpınar.pdf' download className='text-lg text-gray-400'><FaFilePdf /></a>
                 </div>
             </div>
         </Modal>
